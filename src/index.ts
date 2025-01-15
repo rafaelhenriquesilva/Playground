00c270ce690716185f7839f9cab64aaac331ec7f
Rafael Silva <rafaelhenriquesilva494@gmail.com>
2025-01-15 20:26:20 -0300

    get params name to hello

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,21 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.json({
+  extends: true
+}))
 
 // Define a "Hello World" route
 app.get('/', (req: Request, res: Response) => {
   console.info({
     message: 'Consultando o get inicial'
   })
-  const { name } = req.params
+  const name = req.query.name || 'default'
   console.info({
-    message: `params name passado: ${name}`
+    message: `params name passado: ${req.query.name}`
   })
   res.send(`Hello ${name}!`);
 });
